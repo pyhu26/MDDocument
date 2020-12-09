8e67e748ded6b10901499a0574f5f4ccb92d8775
이윤 <pyhu26@gmail.com>
2020-12-09 11:48:27 +0900

    first

---
+++ Services/FileService.cs
@@ -0,0 +1,40 @@
+using System;
+using System.IO;
+using System.Text;
+
+namespace MDDocumnet.Services
+{
+    public class FileService
+    {
+        public void CreateFile()
+        {
+            string path = @"/Users/yoonpro/Projects/MDDocument/MdFiles/MyTest.txt";
+
+            try
+            {
+                // Create the file, or overwrite if the file exists.
+                using (FileStream fs = File.Create(path))
+                {
+                    byte[] info = new UTF8Encoding(true).GetBytes("This is some text in the file.");
+                    // Add some information to the file.
+                    fs.Write(info, 0, info.Length);
+                }
+
+                // Open the stream and read it back.
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
