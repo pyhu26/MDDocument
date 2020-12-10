7a5200558b1dbf39c4bec7e80cfe52b24bf5268f
pyhu26 <pyhu26@gmail.com>
2020-12-10 22:36:19 +0900

    파일수대로 메뉴 생성

---
+++ MdFiles/3 설계/MyTest3.md
@@ -0,0 +1,18 @@
+# 이윤
+# 3. 제목
+* 테스트 불렛 리스트
+>  테스트토글
+
+| 테스트asdfasdf |
+asdfasdf
+asdfa
+
+
+*테스트
+
+--- public ---
+
+
+```public void GetTruck() ```
+
+
---
+++ Services/MDService.cs
@@ -0,0 +1,32 @@
+﻿using Markdig;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MDDocument
+{
+    public class MDService : ComponentBase
+    {
+        [Parameter]
+        public string DirectoryName { get; set; }
+        [Parameter]
+        public string FileName { get; set; }
+        public string ConvertedHtml => Markdown.ToHtml(MarkDownText ?? "please input");
+        public string MarkDownText { get; set; }
+
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+
+            string path = Path.Combine(Environment.CurrentDirectory, "MdFiles", DirectoryName ?? string.Empty,  FileName + ".md");
+            this.MarkDownText = File.ReadAllText(path);
+            
+
+            StateHasChanged();
+        }
+    }
+}
